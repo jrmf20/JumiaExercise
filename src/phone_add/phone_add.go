70fd5313440931fc53909f0d0b0eff5d6bc2944d
João Fontes <1110670@isep.ipp.pt>
2020-02-18 20:57:50 +0000

    Removed coments, added and wired open and close dbconnection functions, formated code

--- src/phone_add/phone_add.go
+++ src/phone_add/phone_add.go
@@ -1,38 +1,43 @@
 package phone_add
 
 import (
+	"bytes"
+	"fmt"
 	"regexp"
 	"strconv"
-	"fmt"
-	"bytes"
 )
-//Country dic structure binding the country name and regex 
-type country_dic struct{
-	name string
+
+//Country dic structure binding the country name and regex
+type country_dic struct {
+	name  string
 	regex *regexp.Regexp
 }
-//map containing code as key and the name/format registry the country names  
+
+//map containing code as key and the name/format registry the country names
 type clist map[int]country_dic
 
 //public variable to the countries
 var PhoneCDic clist
 var regcode *regexp.Regexp
-func init(){
+
+func init() {
 	PhoneCDic = clist{
-		237:country_dic{"Cameroon",regexp.MustCompile(`\((237)\)\ ?([2368]\d{7,8}$)`)},
-	 	251:country_dic{"Ethiopia",regexp.MustCompile(`\((251)\)\ ?([1-59]\d{8}$)`)},
-	 	212:country_dic{"Moroco",regexp.MustCompile(`\((212)\)\ ?([5-9]\d{8}$)`)},
-	 	258:country_dic{"Mozambique",regexp.MustCompile(`\((258)\)\ ?([28]\d{7,8}$)`)},
-	 	256:country_dic{"Uganda",regexp.MustCompile(`\((256)\)\ ?(\d{9}$)`)}} 
+		237: country_dic{"Cameroon", regexp.MustCompile(`\((237)\)\ ?([2368]\d{7,8}$)`)},
+		251: country_dic{"Ethiopia", regexp.MustCompile(`\((251)\)\ ?([1-59]\d{8}$)`)},
+		212: country_dic{"Moroco", regexp.MustCompile(`\((212)\)\ ?([5-9]\d{8}$)`)},
+		258: country_dic{"Mozambique", regexp.MustCompile(`\((258)\)\ ?([28]\d{7,8}$)`)},
+		256: country_dic{"Uganda", regexp.MustCompile(`\((256)\)\ ?(\d{9}$)`)},
+	}
 	regcode = regexp.MustCompile(`\((\d+)\).+`)
 }
 
 //Phone Number "class"
-type PhoneAdd struct{
+type PhoneAdd struct {
 	id, number, code int
-	Name, country string
-	Valid bool
+	Name, country    string
+	Valid            bool
 }
+
 //Populate/Create PhoneAdd "class" given db info
 func CreatePhoneAdd(id int, name string, number string) (p PhoneAdd) {
 	p.id = id
@@ -41,31 +46,31 @@ func CreatePhoneAdd(id int, name string, number string) (p PhoneAdd) {
 	return
 }
 
-//retrives info from db number
-func numberToInfo(n_string string) (country string, code int, number int, valid bool){
-	valid = false	
+//Retrives additional info from db number
+func numberToInfo(n_string string) (country string, code int, number int, valid bool) {
+	valid = false
 	code_string := regcode.FindStringSubmatch(n_string)
-	if code_string==nil {
+	if code_string == nil {
 		//fmt.Println(fmt.Errorf("Invalid Country Code").Error())
 		return
 	}
-	code_int , err := strconv.Atoi(code_string[1])
-	//should always work	
-	if err!=nil {
+	code_int, err := strconv.Atoi(code_string[1])
+	//should always work
+	if err != nil {
 		//fmt.Println(fmt.Errorf("Invalid Country Code").Error())
 		return
 	}
 	code = code_int
-	
-	if count_det, ok := PhoneCDic[code_int]; !ok{
+
+	if count_det, ok := PhoneCDic[code_int]; !ok {
 		//fmt.Println(fmt.Errorf("Unmatched Country Code").Error())
 		return
-	}else{
-		country = count_det.name		
-		if matches := count_det.regex.FindStringSubmatch(n_string); matches !=nil{
-			number_int,err := strconv.Atoi(matches[2])
+	} else {
+		country = count_det.name
+		if matches := count_det.regex.FindStringSubmatch(n_string); matches != nil {
+			number_int, err := strconv.Atoi(matches[2])
 			//should always work
-			if err != nil{
+			if err != nil {
 				//fmt.Println(fmt.Errorf("Unmatched Number Sequence").Error())
 				return
 			}
@@ -73,7 +78,7 @@ func numberToInfo(n_string string) (country string, code int, number int, valid 
 			valid = true
 		}
 	}
-	return	
+	return
 }
 
 func (p *PhoneAdd) MarshalJSON() ([]byte, error) {
@@ -81,10 +86,8 @@ func (p *PhoneAdd) MarshalJSON() ([]byte, error) {
 	buffer.WriteString(fmt.Sprintf("\"id\" : %d,", p.id))
 	buffer.WriteString(fmt.Sprintf("\"name\" : \"%s\",", p.Name))
 	buffer.WriteString(fmt.Sprintf("\"number\" : %d,", p.number))
-	buffer.WriteString(fmt.Sprintf("\"country\" : \"%s\",", p.country))	
+	buffer.WriteString(fmt.Sprintf("\"country\" : \"%s\",", p.country))
 	buffer.WriteString(fmt.Sprintf("\"state\" : %t", p.Valid))
 	buffer.WriteString(fmt.Sprintf("}"))
 	return buffer.Bytes(), nil
 }
-
-
--- src/server/server.go
+++ src/server/server.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"DAO"
+	"context"
 	"encoding/json"
+	_ "fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"phone_add"
 	_ "strconv"
+	"time"
 )
+
 type Phone_List []phone_add.PhoneAdd
 
+//como fazer uma melhor
 func CustomerHandler(w http.ResponseWriter, r *http.Request) {
 	var out []byte
 	switch r.Method {
@@ -21,7 +28,7 @@ func CustomerHandler(w http.ResponseWriter, r *http.Request) {
 				p_list = append(p_list, phone_add.CreatePhoneAdd(cust.ID, cust.Name, cust.Phone))
 			}
 			out, err = json.Marshal(Phone_List(p_list))
-			if err != nil{
+			if err != nil {
 				log.Println(err)
 			}
 		} else {
@@ -37,10 +44,13 @@ func CustomerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	if err := DAO.OpenDBConnection(); err != nil {
+		log.Fatal("Could not connect to db \n Error desc: %s", err)
+	}
+
 	//initialize routes
-	
 	r := mux.NewRouter()
-	
+
 	//main path
 	staticFileDirectory := http.Dir("fe/")
 	staticFileHandler := http.StripPrefix("/fe/", http.FileServer(staticFileDirectory))
@@ -50,5 +60,29 @@ func main() {
 	r.PathPrefix("/scripts/").Handler(http.StripPrefix("/scripts/", http.FileServer(http.Dir("fe/scripts")))).Methods("GET")
 	r.Path("/customer").HandlerFunc(CustomerHandler)
 
-	log.Fatal(http.ListenAndServe(":80", r))
-} //*/
+	server := &http.Server{Addr: ":80", Handler: r}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println("Unnexpected Error: %s", err)
+		}
+	}()
+
+	// Setting up signal capturing
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	// Waiting for SIGINT (pkill -2)
+	<-stop
+
+	if err := DAO.CloseDBConnection(); err != nil {
+		log.Println("Unnexpected error while closing db: %s", err)
+	}
+
+	//Wait
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		// handle err
+	}
+}
